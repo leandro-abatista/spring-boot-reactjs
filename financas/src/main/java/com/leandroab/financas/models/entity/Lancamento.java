1fe45db1cd2c16c90cb4e3aa4a3a378847d39630
Leandro Amorim <leandroamorimpb@gmail.com>
2021-06-22 10:31:53 -0300

    Adicionando o lombok ao projeto

--- financas/src/main/java/com/leandroab/financas/models/entity/Lancamento.java
+++ financas/src/main/java/com/leandroab/financas/models/entity/Lancamento.java
@@ -21,6 +21,23 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import com.leandroab.financas.enums.StatusLancamento;
 import com.leandroab.financas.enums.TipoLancamento;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter//lotação do lombok
+@Setter//lotação do lombok
+@EqualsAndHashCode//lotação do lombok
+@ToString//lotação do lombok
+@NoArgsConstructor//lotação do lombok
+@AllArgsConstructor//lotação do lombok
+@Builder//lotação do lombok
+@Data//lotação do lombok
 @Entity
 @Table(name = "lancamento", schema = "lancamento")
 public class Lancamento implements Serializable {
@@ -60,109 +77,5 @@ public class Lancamento implements Serializable {
 	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento status;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public TipoLancamento getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoLancamento tipo) {
-		this.tipo = tipo;
-	}
-
-	public StatusLancamento getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusLancamento status) {
-		this.status = status;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamento other = (Lancamento) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", descricao=" + descricao + ", mes=" + mes + ", ano=" + ano + ", usuario="
-				+ usuario + ", valor=" + valor + ", dataCadastro=" + dataCadastro + ", tipo=" + tipo + ", status="
-				+ status + "]";
-	}
-	
 	
 }
--- financas/src/main/java/com/leandroab/financas/models/entity/Usuario.java
+++ financas/src/main/java/com/leandroab/financas/models/entity/Usuario.java
@@ -9,6 +9,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter//lotação do lombok
+@Setter//lotação do lombok
+@EqualsAndHashCode//lotação do lombok
+@ToString//lotação do lombok
+@NoArgsConstructor//lotação do lombok
+@AllArgsConstructor//lotação do lombok
+@Builder//lotação do lombok
+@Data//lotação do lombok
 @Entity
 @Table(name = "usuario", schema = "financas")
 public class Usuario implements Serializable {
@@ -28,67 +45,6 @@ public class Usuario implements Serializable {
 
 	@Column(name = "senha")
 	private String senha;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
-	}
+	
 	
 }
